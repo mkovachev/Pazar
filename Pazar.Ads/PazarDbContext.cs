f74cb26615ed324f551868a6146b1018eaec6a91
mkovachev <mkovachev6@gmail.com>
2020-11-06 00:38:04 +0200

    created

---
+++ Pazar.Ads/PazarDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pazar.Ads.Data.Models;
+using System.Reflection;
+
+namespace Pazar.Ads
+{
+    public class PazarDbContext : DbContext
+    {
+        public PazarDbContext(DbContextOptions<PazarDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Ad> Ads { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Image> Images { get; set; }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
