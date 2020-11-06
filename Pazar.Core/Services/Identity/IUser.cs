6859c8a6f880dcd41c608289a843ab45586918d9
mkovachev <mkovachev6@gmail.com>
2020-11-06 16:23:25 +0200

    created

--- Pazar.Core/Services/Identity/IUser.cs
+++ Pazar.Core/Services/Identity/IUser.cs
@@ -1,7 +1,18 @@
-﻿namespace Pazar.Core.Services.Identity
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace Pazar.Core.Services.Identity
 {
     public interface IUser
     {
         string Id { get; }
+
+        string Name { get; }
+
+        bool IsAuthenticated();
+
+        IEnumerable<Claim> GetClaimsIdentity();
+
+        bool IsAdministrator { get; }
     }
 }
---
+++ Pazar.Core/Services/Identity/UserService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace Pazar.Core.Services.Identity
+{
+    internal class UserService : IUser
+    {
+        private readonly IHttpContextAccessor accessor;
+        public UserService(IHttpContextAccessor accessor)
+        {
+            this.accessor = accessor;
+        }
+
+        public string Id => this.accessor.HttpContext?.User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+        public string Name => this.accessor.HttpContext?.User.Identity.Name;
+
+        public bool IsAuthenticated() => this.accessor.HttpContext.User.Identity.IsAuthenticated;
+
+        public IEnumerable<Claim> GetClaimsIdentity() => this.accessor.HttpContext?.User.Claims;
+
+        public bool IsAdministrator => throw new NotImplementedException();
+    }
+}
