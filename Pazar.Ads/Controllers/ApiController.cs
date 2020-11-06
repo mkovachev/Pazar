3036c01e49b9462b4002aef508c09f3bbb26f0bc
mkovachev <mkovachev6@gmail.com>
2020-11-06 15:50:48 +0200

    created

---
+++ Pazar.Ads/Controllers/ApiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pazar.Ads.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public abstract class ApiController : ControllerBase
+    {
+        public const string PathSeparator = "/";
+        public const string Id = "{id}";
+    }
+}
