4f43f5e48b301f6192096d36578101ba96b9efce
mkovachev <mkovachev6@gmail.com>
2020-11-10 15:33:43 +0200

    created

---
+++ Pazar.Watchdog/Startup.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Pazar.Watchdog
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+                .AddHealthChecksUI()
+                .AddInMemoryStorage();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app
+                .UseRouting()
+                .UseEndpoints(endpoints => endpoints
+                    .MapHealthChecksUI(healthChecks => healthChecks
+                        .UIPath = "/healthchecks"));
+        }
+    }
+}
