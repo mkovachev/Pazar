4400e64986fa46018287e7893759db98f070968f
mkovachev <mkovachev6@gmail.com>
2020-11-24 12:21:04 +0200

    created

---
+++ Client/src/app/ads/myads/myads.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-myads',
+  templateUrl: './myads.component.html',
+  styleUrls: ['./myads.component.css']
+})
+export class MyadsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthenticationModule } from './authentication/authentication.module';
+import { CreateComponent } from './ads/create/create.component';
+import { EditComponent } from './ads/edit/edit.component';
+import { MyadsComponent } from './ads/myads/myads.component';
+import { ViewComponent } from './ads/view/view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    CreateComponent,
+    EditComponent,
+    MyadsComponent,
+    ViewComponent,
   ],
   imports: [
     BrowserModule,
