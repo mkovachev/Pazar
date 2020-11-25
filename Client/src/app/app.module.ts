4239834592a0f0f0fd841a4967d27c6a4ea61ce9
mkovachev <mkovachev6@gmail.com>
2020-11-25 16:07:15 +0200

    .

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -4,21 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthenticationModule } from './authentication/authentication.module';
-import { CreateComponent } from './ads
-import { ProfileComponent } from './users/profile/profile.component';
-import { MyAdsComponent } from './my-ads/my-ads.component';
-import { AllAdsComponent } from './all-ads/all-ads.component';
+import { AdsModule } from './ads/ads.module';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MyAdsComponent,
-    AllAdsComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AuthenticationModule
+    AuthenticationModule,
+    AdsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
