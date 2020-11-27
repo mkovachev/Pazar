79fb3443b149710caa322e43551e3bf34e54080e
mkovachev <mkovachev6@gmail.com>
2020-11-27 22:31:14 +0200

    .

--- Client/src/environments/environment.ts
+++ Client/src/environments/environment.ts
@@ -1,8 +1,15 @@
 export const environment = {
   production: true,
-  identityUrl: 'http://34.122.74.94:5001/',
-  adsUrl: 'http://35.184.122.109:5002/',
-  usersUrl: 'http://35.184.122.109:5003/',
-  statisticsUrl: 'http://35.223.2.95:5004/',
-  notificationsUrl: 'http://35.202.99.212:5005/'
+  identityUrl: 'http://localhost:5001/',
+  adsUrl: 'http://localhost:5002/',
+  usersUrl: 'http://localhost:5003/',
+  statisticsUrl: 'http://localhost:5004/',
+  notificationsUrl: 'http://localhost:5005/'
 };
+
+
+// identityUrl: 'http://34.122.74.94:5001/',
+//   adsUrl: 'http://35.184.122.109:5002/',
+//     usersUrl: 'http://35.184.122.109:5003/',
+//       statisticsUrl: 'http://35.223.2.95:5004/',
+//         notificationsUrl: 'http://35.202.99.212:5005/'
