a3fef3ac3fd2b6dbb75ab9f21f25d99f3ade7b61
mkovachev <mkovachev6@gmail.com>
2020-12-04 14:34:18 +0200

    delete modal added

--- Client/src/app/ads/my-ads/my-ads.component.ts
+++ Client/src/app/ads/my-ads/my-ads.component.ts
@@ -33,21 +33,7 @@ export class MyAdsComponent implements OnInit {
   }
 
   open(content: any) {
-    this.modalService.open(content, this.modalOptions).result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
-    }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-    });
-  }
-
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return `with: ${reason}`;
-    }
+    this.modalService.open(content, this.modalOptions)
   }
 
   delete() {
