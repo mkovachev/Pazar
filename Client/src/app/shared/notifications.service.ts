3f162a5daf1869a6ac3afa64b6b1d168cf622c6e
mkovachev <mkovachev6@gmail.com>
2020-12-08 12:54:42 +0200

    .

--- Client/src/app/shared/notifications.service.ts
+++ Client/src/app/shared/notifications.service.ts
@@ -2,12 +2,14 @@ import { Injectable } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import * as signalR from "@aspnet/signalr";
 import { environment } from 'src/environments/environment';
+import { IHttpConnectionOptions } from '@aspnet/signalr';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class NotificationsService {
-  private hubConnection: signalR.HubConnection;
+  private hubConnection!: signalR.HubConnection;
 
   constructor(private toastr: ToastrService) { }
 
@@ -21,7 +23,7 @@ export class NotificationsService {
     };
 
     this.hubConnection = new signalR.HubConnectionBuilder()
-      .withUrl(environment.notificationsUrl + 'notifications', options)
+      .withUrl(environment.notificationsUrl + 'notifications', options as IHttpConnectionOptions)
       .build();
 
     this.hubConnection
@@ -29,9 +31,9 @@ export class NotificationsService {
       .then(() => console.log('Connection started'))
       .catch((err: any) => console.log('Error while starting connection: ' + err));
 
-    this.hubConnection.on('ReceiveNotification', (res: any) => {
-      console.log(res);
-      this.toastr.success(`${res.title} ${res.price}!`, "Ad created");
+    this.hubConnection.on('ReceiveNotification', (data: any) => {
+      console.log(data);
+      this.toastr.success(`${data.title} ${data.price}!`, "Ad created");
     });
   }
 }
